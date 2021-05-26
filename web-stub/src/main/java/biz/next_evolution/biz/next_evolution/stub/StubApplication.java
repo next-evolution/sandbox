66c3ee71b29eabc4071a56744784d0617eb65b7e
next-evolution <ne-dev@next-evolution.co.jp>
2021-05-26 17:00:39 +0900

    [web-stub] initialize.

---
+++ web-stub/src/main/java/biz/next_evolution/biz/next_evolution/stub/StubApplication.java
@@ -0,0 +1,13 @@
+package biz.next_evolution.biz.next_evolution.stub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StubApplication.class, args);
+	}
+
+}
